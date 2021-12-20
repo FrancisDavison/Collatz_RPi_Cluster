15b451467393b11405d72af3d812ac9393631ec4
FrancisDavison <francis.nj.davison@gmail.com>
2021-12-20 12:52:45 +0000

    added transfer class to all other configs

---
+++ src/Se1_Co15_St0/Collatz_Transfer_CSV.java
@@ -0,0 +1,5 @@
+//This class will be called and will serve only as an 'as needed' function to transfer CSV to target destination
+package Se1_Co15_St0;
+public class Collatz_Transfer_CSV
+{
+}
---
+++ src/Se3_Co12_St1/Collatz_Transfer_CSV.java
@@ -0,0 +1,5 @@
+//This class will be called and will serve only as an 'as needed' function to transfer CSV to target destination
+package Se3_Co12_St1;
+public class Collatz_Transfer_CSV
+{
+}
